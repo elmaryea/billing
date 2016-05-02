552feb762003ca07c336116796d185af5e30ac0a
elmaryea <elmaryea@umass.edu>
2016-05-02 16:29:31 -0400

    Set up shell of Payer Test, without content

---
+++ src/test/java/org/maryea/billing/model/PayerTest.java
@@ -0,0 +1,84 @@
+package org.maryea.billing.model;
+
+import java.util.TreeSet;
+import java.util.Set;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PayerTest {
+  
+  @Before
+	public void beforeEachTest() {
+		
+	}
+  
+  @Test
+  public void testGetAccount() {
+    
+  }
+  
+  @Test
+  public void testGetCellPhone() {
+    
+  }
+  
+  @Test
+  public void testGetFirstName() {
+    
+  }
+  
+  @Test
+  public void testGetHomePhone() {
+    
+  }
+  
+  @Test
+  public void testGetId() {
+    
+  }
+  
+  @Test
+  public void testGetLastname() {
+    
+  }
+  
+  @Test
+  public void testGetPayments() {
+    
+  }
+  
+  @Test
+  public void testSetAccount() {
+    
+  }
+  
+  @Test
+  public void testSetCellPhone() {
+    
+  }
+  
+  @Test
+  public void testSetFirstName() {
+    
+  }
+  
+  @Test
+  public void testSetHomePhone() {
+    
+  }
+  
+  @Test
+  public void testSetId() {
+    
+  }
+  
+  @Test
+  public void testSetLastName() {
+    
+  }
+  
+  @Test
+  public void testSetPayments() {
+    
+  }
