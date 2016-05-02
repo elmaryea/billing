1c0d704e01feb8a4a83bad9a696336c127cc9356
elmaryea <elmaryea@umass.edu>
2016-05-02 16:12:17 -0400

    Set up shell of Address Test, without content

---
+++ src/test/java/org/maryea/billing/model/AddressTest.java
@@ -0,0 +1,70 @@
+package org.maryea.billing.model;
+
+import java.util.TreeSet;
+import java.util.Set;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountTest {
+  
+  @Before
+	public void beforeEachTest() {
+		account = new Account(stockAccount);
+	}
+  
+  @Test
+  public void testGetAddress() {
+		
+	}
+	
+	@Test
+	public void testGetCity() {
+		
+	}
+	
+	@Test
+	public void testGetId() {
+		
+	}
+	
+	@Test
+	public void testGetState() {
+		
+	}
+	
+	@Test
+	public void testGetZipCode() {
+		
+	}
+	
+	@Test
+	public void testSetAddress() {
+		
+	}
+	
+	@Test
+	public void testSetCity() {
+		
+	}
+	
+	@Test
+	public void testSetId() {
+		
+	}
+	
+	@Test
+	public void testSetState() {
+		
+	}
+	
+	@Test
+	public void testSetZipCode() {
+		
+	}
+	
+	@Test
+	public void testEquals() {
+		
+	}
+}
