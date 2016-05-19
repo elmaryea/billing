92dd1401ed4a54cf4104c2b22a5c343d58f29069
elmaryea <elmaryea@umass.edu>
2016-05-19 12:24:33 -0400

    Added comments to Account class

--- src/main/java/org/maryea/billing/model/Account.java
+++ src/main/java/org/maryea/billing/model/Account.java
@@ -2,6 +2,15 @@ package org.maryea.billing.model;
 
 import java.util.Set;
 
+/*
+  This class represents an account in the child care program. Each account contains
+  one or two parents (firstName and lastName), their home phone and cellphone, home
+  address, the current account balance, any children they have that are currently 
+  enrolled in the program, a set of all payments that have been made for the account,
+  a set of authorized payers (the parents are automatically made authorized payers),
+  and the program that they belong to (in order to prevent massive method strings to
+  get the program).
+*/
 public class Account{
     private int id;
     private String firstName1, firstName2, lastName1, lastName2, homePhone, cellPhone;
@@ -12,9 +21,16 @@ public class Account{
     private Set<Payer> payers;
     private Business business;
     
+    /*
+      Creates an empty account
+    */
     public Account(){
     }
     
+    /*
+      Creates an account with all information filled in except for the set of kids, set of payments
+      and set of payers
+    */
     public Account(String fn1, String ln1, String fn2, String ln2, String street, String city, String state, String zipCode, String hp, String cp, Business b){
     	balance = 0.0;
     	firstName1 = fn1;
@@ -27,6 +43,9 @@ public class Account{
     	business = b;
     }
     
+    /*
+      Creates a new account with the same information as Account account
+    */
     public Account(Account a){
     	id = a.getId();
     	firstName1 = a.getFirstName1();
@@ -37,9 +56,16 @@ public class Account{
     	cellPhone = a.getCellPhone();
     	address = a.getAddress();
     	business = a.getBusiness();
+      children = a.getChildren();
+      payments = a.getPayments();
+      payers = a.getPayers();
     	balance = 0.0;
     }
     
+    /*
+      Getter methods for each field for Hibernate to use
+    */
+  
     public Address getAddress(){
     	return address;
     }
@@ -92,6 +118,10 @@ public class Account{
     	return payments;
     }
     
+    /*
+      Setter methods for each field for Hibernate to use
+    */
+  
     public void setAddress(Address address){
     	this.address = address;
     }
